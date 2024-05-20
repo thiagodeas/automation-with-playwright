7046d2539c666f4d1b3cd3d04cbec0febe89ffdd
thiagodeas <sousaalvesth@gmail.com>
2024-05-20 11:45:56 -0300

    Documentando todo o código e aplicando Gherkin nos testes

--- tests/application.spec.ts
+++ tests/application.spec.ts
@@ -1,50 +1,68 @@
 import { test, expect } from '@playwright/test'
 import { ApplicationPage } from './support/pages/application'
 
+//Declarando uma variável para armazenar a instância da página
 let appPage: ApplicationPage
 
+//Executa antes de cada teste: cria uma nova instância de 'ApplicationPage' com a página do navegador 
 test.beforeEach(({ page }) => {
     appPage = new ApplicationPage(page)
 })
 
 test('deve conseguir pesquisar um produto', async () => {
 
+    //Dado que eu acesse a página da aplicação
     await appPage.go()
+
+    //Quando eu digitar o nome de um produto e pesquisar
     await appPage.searchProduct('Liquidificador')
+
+    //Então produtos relacionados a pesquisa serão exibidos
     await appPage.searchValidation()
 
 })
 
 test('deve ser obrigatório preencher o campo antes de pesquisar', async () => {
 
+    //Dado que eu acesse a página da aplicação
     await appPage.go()
 
+    //Quando eu tentar pesquisar sem digitar nada no campo de busca de produto
     await appPage.searchProduct('')
 
+    //Então uma mensagem informando que o preenchimento do campo é obrigatório é exibida
     await appPage.requiredFieldValidation()
 
 })
 
 test('deve conseguir adicionar um produto ao carrinho de compras', async () => {
 
+    //Dado que eu acesse a página da aplicação
     await appPage.go()
 
+    //Quando eu clicar no botão para adicionar um produto ao carrinho
     await appPage.addToCart()
 
+    //Então o produto deverá ser adicionado ao carrinho
     await appPage.addToCartValidation()
 
 })
 
 test('deve conseguir remover um produto do carrinho de compras', async () => {
 
+    //Dado que eu acesse a página da aplicação
     await appPage.go()
 
+    //E clico no botão para adicionar um produto ao carrinho
     await appPage.addToCart()
 
+    //E o produto é adicionado ao carrinho
     await appPage.addToCartValidation()
 
+    //Quando eu clicar no botão para remover um produto do carrinho
     await appPage.removeItemToCart()
 
+    //Então o produto deverá ser removido do carrinho
     await appPage.removeValidation()
 
 })
--- tests/support/pages/application/index.ts
+++ tests/support/pages/application/index.ts
@@ -1,16 +1,22 @@
 import { Page, expect } from '@playwright/test'
 
+//Criando e exportando uma classe TypeScript
 export class ApplicationPage {
+
+    //Criando propriedade page e tipando ela com Page
     readonly page: Page
 
+    //Criando uma nova instância e associando ela à página
     constructor(page: Page) {
         this.page = page
     }
 
+    //Criando função que acessa a página da aplicação
     async go() {
         await this.page.goto('/')
     }
 
+    //Criando funçao que pesquisa por um produto
     async searchProduct(productName: string) {
         const inputProductName = this.page.locator('xpath=//form[contains(@class, "sc-eDLKkx")]//input')
         await inputProductName.fill(productName)
@@ -18,19 +24,24 @@ export class ApplicationPage {
         await this.page.click('button[type="submit"]')
     }
 
+    //Criando função que adiciona um produto ao carrinho
     async addToCart () {
         await this.page.click('//section[contains(@class, "sc-iBdnpw")]//div//h2[contains(text(), "Apple iPhone 14 (128 Gb) - Meia Noite - Distribuidor Autorizado")]/../following-sibling::button')
     }
 
+    //Criando função que remove um produto do carrinho
     async removeItemToCart() {
         await this.page.click('xpath=//button[contains(@class, "sc-fHejqy") and contains(@class, "HWKci")]')
     }
 
+
+    //Criando função que valida que o que foi pesquisado foi exibido na tela
     async searchValidation () {
         const target = this.page.locator('xpath=//h2[contains(text(), "Liquidificador Mondial L-99 500w 2,2l C/ Jarra San Pt - 110v")]')
         await target.isVisible()
     }
 
+    //Criando função que valida que o preenchimento do campo de busca é obrigatório
     async requiredFieldValidation () {
         const inputProductName = this.page.locator('xpath=//form[contains(@class, "sc-eDLKkx")]//input')
 
@@ -38,6 +49,7 @@ export class ApplicationPage {
         await expect(validationMessage === 'Preencha este campo.' || validationMessage === 'Please fill out this field.').toBe(true);
     }
 
+    //Criando função que valida a inclusão de um produto ao carrinho
     async addToCartValidation() {
         const target = this.page.locator('xpath=//button[contains(@class,"sc-dmyCSP")]//span')
         await expect(target).toBeVisible()
@@ -46,6 +58,7 @@ export class ApplicationPage {
         await expect(totalCartValue).not.toHaveText('R$ 0,00')
     }
 
+    //Criando função que valida a remoção de um produto do carrinho
     async removeValidation() {
         const target = this.page.locator('xpath=//button[contains(@class,"sc-dmyCSP")]//span')
         await expect(target).not.toBeVisible()
